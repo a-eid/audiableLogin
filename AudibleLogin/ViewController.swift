45802bd425ba91d9748311a9d89e1e788bef13dd
Ahmed Eid <a.eid@yandex.com>
2018-03-18 23:04:06 +0300

    Initial Commit

---
+++ AudibleLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudibleLogin
+//
+//  Created by Ahmed Eid on 18/03/2018.
+//  Copyright © 2018 Ahmed Eid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
